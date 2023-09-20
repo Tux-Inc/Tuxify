a4ba84b3fda7f236474138176d50b68a9442f6f0
Neeptossss <gwenael.hubler@epitech.eu>
2023-09-20 18:30:01 +0200

    refacto(compo): add names for buttons compo page

--- frontend/pages/components.vue
+++ frontend/pages/components.vue
@@ -7,16 +7,16 @@
     <div class="flex flex-col">
         <h2 class="text-2xl font-bold mb-8">Buttons</h2>
         <div class="flex flex-col gap-2">
-          <Button type="primary" text="Click me" icon="bx bx-user" :onClick="() => {}" :state="'idle'"></Button>
-          <Button type="secondary" text="Click me" icon="bx bx-user" :onClick="() => {}" :state="'idle'"></Button>
-          <Button type="success" text="Click me" icon="bx bx-user" :onClick="() => {}" :state="'idle'"></Button>
-          <Button type="warning" text="Click me" icon="bx bx-user" :onClick="() => {}" :state="'idle'"></Button>
-          <Button type="danger" text="Click me" icon="bx bx-user" :onClick="() => {}" :state="'idle'"></Button>
-          <Button type="info" text="Click me" icon="bx bx-user" :onClick="() => {}" :state="'idle'"></Button>
-          <Button type="primary" text="Click me" icon="bx bx-user" :onClick="() => {}" :state="'loading'"></Button>
-          <Button type="primary" text="Click me" icon="bx bx-user" :onClick="() => {}" :state="'disabled'"></Button>
-          <Button type="primary" :outlined="true" text="Click me" icon="bx bx-user" :onClick="() => {}" :state="'idle'"></Button>
-          <Button type="secondary" :outlined="true" text="Click me" icon="bx bx-user" :onClick="() => {}" :state="'idle'"></Button>
+          <Button type="primary" text="Primary filled button" icon="bx bx-user" :onClick="() => {}" :state="'idle'"></Button>
+          <Button type="secondary" text="Secondary filled button" icon="bx bx-user" :onClick="() => {}" :state="'idle'"></Button>
+          <Button type="success" text="Success filled button" icon="bx bx-check-circle" :onClick="() => {}" :state="'idle'"></Button>
+          <Button type="warning" text="Warning filled button" icon="bx bx-error-circle" :onClick="() => {}" :state="'idle'"></Button>
+          <Button type="danger" text="Danger filled button" icon="bx bx-trash" :onClick="() => {}" :state="'idle'"></Button>
+          <Button type="info" text="Info filled button" icon="bx bx-info-circle" :onClick="() => {}" :state="'idle'"></Button>
+          <Button type="primary" text="Primary loading button" icon="bx bx-user" :onClick="() => {}" :state="'loading'"></Button>
+          <Button type="primary" text="Primary disabled button" icon="bx bx-user" :onClick="() => {}" :state="'disabled'"></Button>
+          <Button type="primary" :outlined="true" text="Primary outlined button" icon="bx bx-user" :onClick="() => {}" :state="'idle'"></Button>
+          <Button type="secondary" :outlined="true" text="Secondary outlined button" icon="bx bx-user" :onClick="() => {}" :state="'idle'"></Button>
         </div>
     </div>
   </div>
