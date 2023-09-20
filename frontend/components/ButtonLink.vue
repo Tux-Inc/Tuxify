5d5836cc9c3dc0920f4b3419fa34755b01ef6d54
Neeptossss <gwenael.hubler@epitech.eu>
2023-09-20 19:55:20 +0200

    feat(compo): buttonLink add size

--- frontend/components/ButtonLink.vue
+++ frontend/components/ButtonLink.vue
@@ -3,14 +3,22 @@ import {IButtonLinkProps} from "../types/IButtonLinkProps";
 const props = withDefaults(defineProps<IButtonLinkProps>(), {
     text: 'Link',
     iconPosition: 'left',
+    size: 'medium',
     onClick: () => console.log('Default link click handler'),
 });
+
+const buttonSizeStyleMap = {
+    small: 'px-2 py-1 text-sm',
+    medium: 'px-4 py-2 text-md',
+    large: 'px-6 py-3 text-lg',
+}
 </script>
 
 <template>
     <button
         @click.prevent="props.onClick"
-        class="flex items-center justify-center gap-2 px-4 py-2 text-md rounded-lg cursor-pointer transition duration-50 ease-in-out text-dark dark:text-light hover:bg-primary-500 hover:bg-opacity-10 dark:hover:bg-primary-500 dark:hover:bg-opacity-10"
+        class="flex items-center justify-center gap-2 text-md rounded-lg cursor-pointer transition duration-50 ease-in-out text-dark dark:text-light hover:bg-primary-500 hover:bg-opacity-10 dark:hover:bg-primary-500 dark:hover:bg-opacity-10"
+        :class="buttonSizeStyleMap[props.size]"
     >
         <div v-if="props.iconPosition === 'left'">
             <i :class="props.icon"></i>
--- frontend/types/IButtonLinkProps.ts
+++ frontend/types/IButtonLinkProps.ts
@@ -1,6 +1,7 @@
 export interface IButtonLinkProps {
     text: string;
     icon?: string;
+    size?: 'small' | 'medium' | 'large',
     iconPosition?: 'left' | 'right',
     onClick: () => void;
 }
