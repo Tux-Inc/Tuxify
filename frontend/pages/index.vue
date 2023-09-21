e08a8b11e2ac40f2d44b59043cf874a0a0238a77
Neeptossss <gwenael.hubler@epitech.eu>
2023-09-21 16:42:39 +0200

    feat(view): add settings pages

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="flex items-start justify-between gap-4">
-      <h1 class="text-4xl font-bold text-dark dark:text-light">Welcome, John Doe</h1>
+    <div class="flex flex-wrap items-center justify-between gap-4">
+      <h1 class="text-4xl font-bold text-dark dark:text-light">Home</h1>
       <Button type="primary" text="New flow" icon="bx bx-plus" :onClick="setLoading" :state="loading ? 'idle' : 'loading'"></Button>
     </div>
 </template>
