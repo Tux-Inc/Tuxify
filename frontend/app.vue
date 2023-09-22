bd95f8630e22732e8cbfca85eb255e2a0648d426
Neeptossss <gwenael.hubler@epitech.eu>
2023-09-22 16:09:29 +0200

    refacto(view): wrap main.vue to a default NuxtLayout

--- frontend/app.vue
+++ frontend/app.vue
@@ -4,28 +4,6 @@ definePageMeta({
 })
 </script>
 
-<template>
-  <div class="min-h-screen custom-bg md:p-2">
-    <div class="h-full md:rounded-lg md:p-2 bg-light dark:bg-dark shadow-lg backdrop-blur-lg backdrop-filter bg-opacity-30 dark:bg-opacity-30 md:border md:border-dark md:dark:border-light md:border-opacity-3 md:dark:border-opacity-30">
-      <NuxtLayout name="navigation">
-          <div class="md:rounded-lg flex flex-col min-h-[calc(100vh-80px)] max-h-[calc(100vh-80px)] md:max-h-[calc(100vh-85px)] md:min-h-[calc(100vh-85px)] overflow-y-auto w-full h-screen px-4 pb-4 pt-14 md:p-12 bg-light dark:bg-dark backdrop-blur-lg backdrop-filter bg-opacity-60 dark:bg-opacity-60">
-            <NuxtPage />
-          </div>
-      </NuxtLayout>
-    </div>
-  </div>
-</template>
-
-
-<style scoped>
-.custom-bg {
-  background-image: url('/bg.svg');
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
-}
-</style>
-
 <style>
 .page-enter-active,
 .page-leave-active {
@@ -59,3 +37,14 @@ definePageMeta({
     display: none;
 }
 </style>
+
+<template>
+  <NuxtLayout>
+    <NuxtLayout name="navigation">
+        <div class="md:rounded-lg flex flex-col min-h-[calc(100vh-80px)] max-h-[calc(100vh-80px)] md:max-h-[calc(100vh-85px)] md:min-h-[calc(100vh-85px)] overflow-y-auto w-full h-screen px-4 pb-4 pt-14 md:p-12 bg-light dark:bg-dark backdrop-blur-lg backdrop-filter bg-opacity-60 dark:bg-opacity-60">
+          <NuxtPage />
+        </div>
+    </NuxtLayout>
+  </NuxtLayout>
+</template>
+
---
+++ frontend/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="min-h-screen custom-bg md:p-2">
+        <div class="h-full md:rounded-lg md:p-2 bg-light dark:bg-dark shadow-lg backdrop-blur-lg backdrop-filter bg-opacity-30 dark:bg-opacity-30 md:border md:border-dark md:dark:border-light md:border-opacity-3 md:dark:border-opacity-30">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.custom-bg {
+    background-image: url('/bg.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+}
+</style>
