12bd2437040858cb140fb553540fd1c37b7cdd5d
Neeptossss <gwenael.hubler@epitech.eu>
2023-09-22 17:00:54 +0200

    fix(layout): change default to fixed (prevent scrolling)

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="min-h-screen custom-bg md:p-2">
+    <div class="fixed w-screen min-h-screen custom-bg md:p-2">
         <div class="h-full md:rounded-lg md:p-2 bg-light dark:bg-dark shadow-lg backdrop-blur-lg backdrop-filter bg-opacity-30 dark:bg-opacity-30 md:border md:border-dark md:dark:border-light md:border-opacity-3 md:dark:border-opacity-30">
             <slot />
         </div>
