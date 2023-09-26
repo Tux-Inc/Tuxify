15d52b76ad03b000be05f91a5b1ef070d444baa4
Neeptossss <gwenael.hubler@epitech.eu>
2023-09-26 18:13:35 +0200

    feat(view): translating home hero

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
+import {useI18n} from "vue-i18n";
+
 definePageMeta({
     layout: 'navigation',
 })
+
+const i18n = useI18n()
 </script>
 
 <template>
@@ -9,15 +13,13 @@ definePageMeta({
         <div class="py-24 sm:py-32 md:py-40 relative mb-[calc(var(--header-height)*2)]">
             <div class="mx-auto px-4 sm:px-6 lg:px-8 max-w-3xl">
                 <div class="relative z-[1] text-center">
-                    <h1 class="text-4xl font-extrabold text-dark dark:text-light sm:text-5xl md:text-6xl">
-                        Get more than an <span class="text-primary">automation</span> tool
-                    </h1>
+                    <h1 class="text-4xl font-extrabold text-dark dark:text-light sm:text-5xl md:text-6xl" v-html="i18n.t('landing.home.hero.title')" />
                     <p class="mt-6 text-lg tracking-tight text-gray-600 dark:text-gray-300">
-                        Tuxify is a platform that helps you to automate your business processes and workflows, so you can focus on what really matters.
+                        {{ i18n.t('landing.home.hero.description') }}
                     </p>
                     <div class="mt-10 flex flex-wrap gap-x-6 gap-y-3 justify-center">
-                        <UButton icon="i-heroicons-rocket-launch" size="lg" to="/sign-in" color="primary" variant="solid">Get started</UButton>
-                        <UButton icon="i-mdi-github" size="lg" to="https://github.com/tux-inc/Tuxify" target="_blank" color="white" variant="solid">View on GitHub</UButton>
+                        <UButton icon="i-heroicons-rocket-launch" size="lg" to="/sign-in" color="primary" variant="solid">{{ i18n.t('landing.home.hero.getStarted') }}</UButton>
+                        <UButton icon="i-mdi-github" size="lg" to="https://github.com/tux-inc/Tuxify" target="_blank" color="white" variant="solid">{{ i18n.t('landing.home.hero.viewOnGitHub') }}</UButton>
                     </div>
                 </div>
             </div>
