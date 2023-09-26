feddc749646461d5d274400046aeb783f522414f
Neeptossss <gwenael.hubler@epitech.eu>
2023-09-26 13:29:18 +0200

    refacto(compo): wrap mobile menu height in css variable

--- frontend/components/MobileNavigator.vue
+++ frontend/components/MobileNavigator.vue
@@ -25,7 +25,7 @@ const menus = [
 
 <template>
     <header class="sticky bottom-0 z-50 border-t border-gray-200 dark:border-gray-800 bg-base-light/75 dark:bg-base-dark/75 dark:bg-opacity-60 backdrop-blur">
-        <div class="flex items-start justify-between min-h-[80px] max-h-[80px] px-4 bg-light dark:bg-dark">
+        <div class="flex items-start justify-between min-h-[--mobile-menu-height] max-h-[--mobile-menu-height] px-4 bg-light dark:bg-dark">
             <MobileNavigationButton v-for="menu in menus" :key="menu.name" :text="menu.name" :icon="menu.icon" :onClick="() => navigateTo(menu.path)"></MobileNavigationButton>
         </div>
     </header>
