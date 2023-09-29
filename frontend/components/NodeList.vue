57a0d027b9a2ead213b0b8cfb96331b5f34d3854
Neeptossss <gwenael.hubler@epitech.eu>
2023-09-29 18:04:36 +0200

    fix(compo): cursor-pointer for nodeLisstElement

--- frontend/components/NodeList.vue
+++ frontend/components/NodeList.vue
@@ -10,7 +10,7 @@ defineProps({
 </script>
 
 <template>
-    <aside class="rounded-lg bg-base-light dark:bg-gray-800 p-4 flex flex-col gap-4 m-2 absolute z-10 bg-opacity-90 dark:bg-opacity-90">
+    <aside class="rounded-lg bg-white dark:bg-gray-800 p-4 flex flex-col gap-4 m-2 absolute z-10 bg-opacity-90 dark:bg-opacity-90">
         <div class="text-dark dark:text-light font-bold text-xl">Available nodes</div>
         <UInput icon="i-heroicons-magnifying-glass-20-solid" placeholder="Search..." size="sm" color="white" :trailing="false" />
         <div class="flex flex-col gap-2">
