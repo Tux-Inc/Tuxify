1b0744dab8b0d33bfa63f49a88ce2ccb2d13cbe2
Neeptossss <gwenael.hubler@epitech.eu>
2023-09-29 15:46:15 +0200

    feat(node): add NodeText

--- frontend/components/NodeListElement.vue
+++ frontend/components/NodeListElement.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-function onDragStart(event, nodeType) {
+function onDragStart(event: any, nodeType: any) {
     if (event.dataTransfer) {
         event.dataTransfer.setData('application/vueflow', nodeType)
         event.dataTransfer.effectAllowed = 'move'
@@ -9,7 +9,7 @@ function onDragStart(event, nodeType) {
 
 <template>
   <UTooltip text="Trigger when you receive an email">
-    <div :draggable="true" @dragstart="onDragStart($event, 'input')" class="flex flex-row items-center justify-start gap-2 w-full p-2 rounded-lg border border-gray-300 dark:border-gray-700 bg-white dark:bg-base-dark">
+    <div :draggable="true" @dragstart="onDragStart($event, 'text')" class="flex flex-row items-center justify-start gap-2 w-full p-2 rounded-lg border border-gray-300 dark:border-gray-700 bg-white dark:bg-base-dark">
       <UIcon name="i-mdi-google" class="text-gray-500 dark:text-gray-300" />
       <span class="text-gray-500 dark:text-gray-300">Gmail</span>
     </div>
