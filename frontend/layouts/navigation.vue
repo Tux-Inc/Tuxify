1fe6e4e09abfec178466f11528b6a7723a5e27e8
AlexandreDFM <alexandre-kevin.de-freitas-martins@epitech.eu>
2023-10-07 01:41:57 +0200

    fix(lighthouse): display images with incorrect aspect ratio

--- frontend/layouts/navigation.vue
+++ frontend/layouts/navigation.vue
@@ -12,7 +12,7 @@ const setOpened = () => isOpen.value = !isOpen.value;
         <img src="/hero.webp" alt="hero"
              class="dark:hidden inset-0 w-screen h-[20rem] left-0 overflow-hidden absolute object-fit" />
         <img src="/hero-dark.webp" alt="hero"
-             class="hidden dark:block inset-0 w-screen h-[20rem] left-0 overflow-hidden absolute object-fit" />
+             class="hidden dark:block mx-auto inset-0 h-[20rem] left-0 overflow-hidden absolute object-fit" />
         <div class="hidden md:block">
             <Navbar :isOpen="isOpen" @setOpened="setOpened" />
         </div>
