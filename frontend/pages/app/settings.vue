6b50158ffff04f91d3b68d90d6e9c0b5b8a992d9
AlexandreDFM <alexandre-kevin.de-freitas-martins@epitech.eu>
2023-10-09 22:39:26 +0200

    refactor(settings): format code & change style

--- frontend/pages/app/settings.vue
+++ frontend/pages/app/settings.vue
@@ -1,31 +1,34 @@
 <script setup lang="ts">
 definePageMeta({
-    layout: 'app-navigation',
-})
-let isLoading = ref(false)
+    layout: "app-navigation",
+});
+let isLoading = ref(false);
+
 function signOut() {
-    isLoading.value = true
+    isLoading.value = true;
     setTimeout(() => {
-        isLoading.value = false
-    }, 1000)
+        isLoading.value = false;
+    }, 1000);
 }
 </script>
 <template>
     <div>
         <div class="flex flex-wrap items-center justify-between gap-4">
-            <h1 class="text-4xl font-bold text-dark dark:text-light">Settings</h1>
+            <h1 class="text-4xl font-bold text-dark dark:text-light hidden sm:block">Settings</h1>
         </div>
-        <div class="flex flex-col gap-4 mt-14">
+        <div class="flex flex-col gap-4">
             <span class="text-dark dark:text-light font-bold text-2xl">My account</span>
             <div class="flex items-center gap-2">
-                <img src="/avatar/3.png" class="rounded-full h-20"/>
+                <img src="/avatar/3.png" class="rounded-full h-20" />
                 <div class="flex flex-col">
                     <span class="text-dark dark:text-light font-bold text-xl">John Doe</span>
                     <span class="text-dark dark:text-light">Basic user</span>
                 </div>
             </div>
-            <div>
-                <UButton icon="i-heroicons-arrow-left-on-rectangle" color="red" variant="solid" @click="signOut" :loading="isLoading">Sign out</UButton>
+            <div class="text-center">
+                <UButton icon="i-heroicons-arrow-left-on-rectangle" color="red" variant="solid" @click="signOut"
+                         :loading="isLoading">Sign out
+                </UButton>
             </div>
         </div>
     </div>
