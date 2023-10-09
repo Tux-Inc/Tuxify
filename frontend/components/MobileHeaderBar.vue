948d1fee299b17ca782ea9cf004d110d5ecbffd8
AlexandreDFM <alexandre-kevin.de-freitas-martins@epitech.eu>
2023-10-09 11:05:26 +0200

    feature(mobile): headerbar and bottom bar responsive ui for mobile

---
+++ frontend/components/MobileHeaderBar.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+const menus = [
+    {
+        name: "Home",
+        icon: "bx bx-home",
+        path: "/app",
+    },
+    {
+        name: "Flows",
+        icon: "bx bx-link",
+        path: "/app/flows",
+    },
+    {
+        name: "Services",
+        icon: "bx bx-server",
+        path: "/app/services",
+    },
+    {
+        name: "Settings",
+        icon: "bx bx-cog",
+        path: "/app/settings",
+    },
+];
+</script>
+
+<template>
+    <header
+        class="fixed w-screen top-0 z-50 border-t border-gray-200 dark:border-gray-800 bg-base-light/75 dark:bg-base-dark/75 dark:bg-opacity-60 backdrop-blur">
+        <div
+            class="flex items-center justify-center min-h-[--mobile-menu-height] max-h-[--mobile-menu-height] px-4 bg-light dark:bg-dark">
+            <h1 class="text-4xl font-bold text-dark dark:text-light">
+                Services
+            </h1>
+        </div>
+    </header>
+</template>
--- frontend/layouts/app-navigation.vue
+++ frontend/layouts/app-navigation.vue
@@ -8,62 +8,100 @@ const isCommandPaletteOpen = ref(false);
 const isNewFlowModalOpen = ref(false);
 
 const sendEvent = (event: string) => {
-    isCommandPaletteOpen.value = false
-    $event(event)
-}
+    isCommandPaletteOpen.value = false;
+    $event(event);
+};
 
 defineShortcuts({
     meta_k: {
         usingInput: true,
         handler: () => {
-            isCommandPaletteOpen.value = !isCommandPaletteOpen.value
-        }
+            isCommandPaletteOpen.value = !isCommandPaletteOpen.value;
+        },
     },
     meta_shift_f: {
         usingInput: true,
         handler: () => {
-            isNewFlowModalOpen.value = !isNewFlowModalOpen.value
-        }
-    }
-})
+            isNewFlowModalOpen.value = !isNewFlowModalOpen.value;
+        },
+    },
+});
 
-$listen('app:newFlow', () => {
-    isNewFlowModalOpen.value = !isNewFlowModalOpen.value
-})
+$listen("app:newFlow", () => {
+    isNewFlowModalOpen.value = !isNewFlowModalOpen.value;
+});
 
-$listen('app:commandPalette', () => {
-    isCommandPaletteOpen.value = !isCommandPaletteOpen.value
-})
+$listen("app:commandPalette", () => {
+    isCommandPaletteOpen.value = !isCommandPaletteOpen.value;
+});
 
 const users = [
-    { id: 'benjamincanac', label: 'benjamincanac', href: 'https://github.com/benjamincanac', target: '_blank', avatar: { src: 'https://avatars.githubusercontent.com/u/739984?v=4' } },
-    { id: 'Atinux', label: 'Atinux', href: 'https://github.com/Atinux', target: '_blank', avatar: { src: 'https://avatars.githubusercontent.com/u/904724?v=4' } },
-    { id: 'smarroufin', label: 'smarroufin', href: 'https://github.com/smarroufin', target: '_blank', avatar: { src: 'https://avatars.githubusercontent.com/u/7547335?v=4' } }
+    {
+        id: "benjamincanac",
+        label: "benjamincanac",
+        href: "https://github.com/benjamincanac",
+        target: "_blank",
+        avatar: { src: "https://avatars.githubusercontent.com/u/739984?v=4" },
+    },
+    {
+        id: "Atinux",
+        label: "Atinux",
+        href: "https://github.com/Atinux",
+        target: "_blank",
+        avatar: { src: "https://avatars.githubusercontent.com/u/904724?v=4" },
+    },
+    {
+        id: "smarroufin",
+        label: "smarroufin",
+        href: "https://github.com/smarroufin",
+        target: "_blank",
+        avatar: { src: "https://avatars.githubusercontent.com/u/7547335?v=4" },
+    },
 ];
 const actions = [
-    { id: 'new-flow', label: 'Create a new flow', icon: 'i-heroicons-link', click: () => sendEvent('app:newFlow'), shortcuts: [metaSymbol, 'shift', 'F'] },
-    { id: 'new-team', label: 'Create a new team', icon: 'i-heroicons-user-group', click: () => toast.add({ title: 'New team added!' }), shortcuts: [metaSymbol, 'shift', 'T'] },
-    { id: 'report', label: 'Report a bug', icon: 'i-heroicons-bug-ant', click: () => toast.add({ title: 'Bug reported!' }), shortcuts: ['⌘', 'R'] },
+    {
+        id: "new-flow",
+        label: "Create a new flow",
+        icon: "i-heroicons-link",
+        click: () => sendEvent("app:newFlow"),
+        shortcuts: [metaSymbol, "shift", "F"],
+    },
+    {
+        id: "new-team",
+        label: "Create a new team",
+        icon: "i-heroicons-user-group",
+        click: () => toast.add({ title: "New team added!" }),
+        shortcuts: [metaSymbol, "shift", "T"],
+    },
+    {
+        id: "report",
+        label: "Report a bug",
+        icon: "i-heroicons-bug-ant",
+        click: () => toast.add({ title: "Bug reported!" }),
+        shortcuts: ["⌘", "R"],
+    },
 ];
+
 const groups = computed(() =>
     [commandPaletteRef.value?.query ? {
-        key: 'users',
-        commands: users
+        key: "users",
+        commands: users,
     } : {
-        key: 'recent',
-        label: 'Recent searches',
-        commands: users.slice(0, 1)
+        key: "recent",
+        label: "Recent searches",
+        commands: users.slice(0, 1),
     }, {
-        key: 'actions',
-        commands: actions
+        key: "actions",
+        commands: actions,
     }].filter(Boolean));
-function onSelect (option: any) {
+
+function onSelect(option: any) {
     if (option.click) {
-        option.click()
+        option.click();
     } else if (option.to) {
-        router.push(option.to)
+        router.push(option.to);
     } else if (option.href) {
-        window.open(option.href, '_blank')
+        window.open(option.href, "_blank");
     }
 }
 
@@ -72,7 +110,10 @@ function onSelect (option: any) {
 <template>
     <div class="antialiased min-h-screen bg-base-light dark:bg-base-dark">
         <div class="md:hidden">
-            <slot />
+            <MobileHeaderBar />
+            <div class="p-8 min-h-[--mobile-menu-height] max-h-[--mobile-menu-height]">
+                <slot />
+            </div>
             <MobileNavigator />
         </div>
         <div class="hidden md:block">
