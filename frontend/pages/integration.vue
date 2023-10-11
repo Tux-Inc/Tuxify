cab338ae068cdcdd9a586464336cf7126e4b9190
Sfievez <stephane.fievez@epitech.eu>
2023-10-11 18:10:37 +0200

    feat(compo): fix responsive integration page

--- frontend/pages/integration.vue
+++ frontend/pages/integration.vue
@@ -1,38 +1,42 @@
 <script setup lang="ts" xmlns="http://www.w3.org/1999/html">
-import {useI18n} from "vue-i18n";
-import { IIntegrationList} from "~/types/IIntegrationList";
+import { useI18n } from "vue-i18n";
+import { IIntegrationList } from "~/types/IIntegrationList";
 
 definePageMeta({
-  layout: 'navigation',
+    layout: 'navigation',
 })
 
 const i18n = useI18n()
 let integrations = ref<IIntegrationList[]>(i18n.tm<IIntegrationList[]>('landing.integration.services') as IIntegrationList[]);
 onUpdated(() => {
-  integrations.value = i18n.tm<IIntegrationList[]>('landing.integration.services') as IIntegrationList[];
-})
+    integrations.value = i18n.tm<IIntegrationList[]>('landing.integration.services') as IIntegrationList[];
+});
 
 </script>
 
 <template>
-  <div class="py-24 sm:py-32 md:py-40 relative mb-[calc(var(--header-height)*2)]">
-    <div class="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">
-      <div class="relative z-[1] text-center">
-        <h1 class="text-4xl font-extrabold text-dark dark:text-light sm:text-5xl md:text-6xl" v-html="i18n.t('landing.integration.title')" />
-        <p class="mt-6 text-lg tracking-tight text-gray-600 dark:text-gray-300">
-          {{ i18n.t('landing.integration.description') }}
-        </p>
-      </div>
-    </div>
-      <div class="mt-10 flex flex-wrap gap-x-6 gap-y-10 justify-center max text-center">
-        <div class="container mx-auto my-auto grid grid-cols-3 gap-8">
-          <IntegrationList
-            v-for="(integration) in integrations"
-            :title="integration.title"
-            :description="integration.description"
-            :icon="integration.icon"
-          />
+    <div class="py-24 sm:py-32 md:py-40 relative mb-[calc(var(--header-height)*2)]">
+        <div class="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">
+            <div class="relative z-[1] text-center">
+                <h1 class="text-4xl font-extrabold text-dark dark:text-light sm:text-5xl md:text-6xl" v-html="i18n.t('landing.integration.title')" />
+                <p class="mt-6 text-lg tracking-tight text-gray-600 dark:text-gray-300">
+                    {{ i18n.t('landing.integration.description') }}
+                </p>
+            </div>
         </div>
-      </div>
-  </div>
+        <div class="mt-10 flex flex-wrap justify-centermax text-center mx-auto my-auto grid grid-cols-1 gap-8 sm:grid-cols-3 text-5xl md:text-6xl">
+            <IntegrationList
+                v-for="(integration, index) in integrations"
+                :class="`${
+                       index === integrations.length - 1 &&
+                       integrations.length % 3 !== 0
+                           ? 'md:col-start-2'
+                           : ''
+                    }`"
+                :title="integration.title"
+                :description="integration.description"
+                :icon="integration.icon"
+            />
+        </div>
+    </div>
 </template>
