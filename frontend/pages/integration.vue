8721b7807118d20c312377bb93d41a77e948c0b8
AlexandreDFM <alexandre-kevin.de-freitas-martins@epitech.eu>
2023-10-15 17:49:32 +0200

    hotfix(integration): fix condition for pair or multiple of 3 services

--- frontend/pages/integration.vue
+++ frontend/pages/integration.vue
@@ -1,24 +1,26 @@
-<script setup lang="ts" xmlns="http://www.w3.org/1999/html">
+<script lang="ts" setup xmlns="http://www.w3.org/1999/html">
 import { useI18n } from "vue-i18n";
 import { IIntegrationList } from "~/types/IIntegrationList";
 
 definePageMeta({
-    layout: 'navigation',
-})
+    layout: "navigation",
+});
 
-const i18n = useI18n()
-let integrations = ref<IIntegrationList[]>(i18n.tm<IIntegrationList[]>('landing.integration.services') as IIntegrationList[]);
+const i18n = useI18n();
+let integrations = ref<IIntegrationList[]>(i18n.tm<IIntegrationList[]>("landing.integration.services") as IIntegrationList[]);
 onUpdated(() => {
-    integrations.value = i18n.tm<IIntegrationList[]>('landing.integration.services') as IIntegrationList[];
+    integrations.value = i18n.tm<IIntegrationList[]>("landing.integration.services") as IIntegrationList[];
 });
 
 function checkIntegration(integration: IIntegrationList[], index: number) {
     if (index === integration.length - 1 && integration.length % 3 === 0) {
-        return '';
-    } else if (index === integration.length - 2 && integration.length % 2 !== 0) {
-        return 'md:col-start-2';
+        return "";
+    } else if (index === integration.length - 2 && integration.length % 2 === 0 && integration.length % 3 !== 0) {
+        return "md:col-start-2";
+    } else if (index === integration.length - 1 && integration.length % 3 === 0) {
+        return "md:col-start-2";
     } else if (index === integration.length - 1 && (integration.length + 1) % 3 !== 0) {
-        return 'md:col-start-3';
+        return "md:col-start-3";
     }
 }
 
@@ -28,9 +30,10 @@ function checkIntegration(integration: IIntegrationList[], index: number) {
     <div class="py-24 sm:py-32 md:py-40 relative mb-[calc(var(--header-height)*2)]">
         <div class="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">
             <div class="relative z-[1] text-center">
-                <h1 class="text-4xl font-extrabold text-dark dark:text-light sm:text-5xl md:text-6xl" v-html="i18n.t('landing.integration.title')" />
+                <h1 class="text-4xl font-extrabold text-dark dark:text-light sm:text-5xl md:text-6xl"
+                    v-html="i18n.t('landing.integration.title')" />
                 <p class="mt-6 text-lg tracking-tight text-gray-600 dark:text-gray-300">
-                    {{ i18n.t('landing.integration.description') }}
+                    {{ i18n.t("landing.integration.description") }}
                 </p>
             </div>
         </div>
@@ -38,13 +41,11 @@ function checkIntegration(integration: IIntegrationList[], index: number) {
             class="mt-10 flex flex-wrap justify-centermax text-center mx-auto my-auto grid sm:grid-cols-1 gap-8 md:grid-cols-6 text-5xl md:text-6xl">
             <IntegrationList
                 v-for="(integration, index) in integrations"
-                :class="`${
-                       checkIntegration(integrations, index)
-                    }`"
-                class="md:col-span-2 flex flex-col justify-center"
-                :title="integration.title"
+                :class="`${checkIntegration(integrations, index)}`"
                 :description="integration.description"
                 :icon="integration.icon"
+                :title="integration.title"
+                class="md:col-span-2 flex flex-col justify-center"
             />
         </div>
     </div>
