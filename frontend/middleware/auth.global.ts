0a199a6b0bd4efd3a56e4ecbf8a503c102c48bf8
Neeptossss <gwenael.hubler@epitech.eu>
2023-10-16 23:40:18 +0200

    feat(auth): auto login when cookie present

--- frontend/middleware/auth.global.ts
+++ frontend/middleware/auth.global.ts
@@ -1,11 +1,18 @@
+import { IUserCookie } from "~/types/IUserCookie";
+import { CookieRef } from "#app";
+
 export default defineNuxtRouteMiddleware(async (context) => {
-    const userCookie = useCookie('user');
+    const userCookie: CookieRef<IUserCookie> = useCookie<IUserCookie>('user');
     const router = useRouter();
     if (context.path.startsWith('/app')) {
-        if (!toRaw(userCookie.value)) {
-            console.log('User not logged in');
+        if (!userCookie.value) {
             await router.push('/auth/sign-in');
         }
         // TODO: check if the access token is expired
+    }
+    if (context.path.startsWith('/auth')) {
+        if (userCookie.value) {
+            await router.push('/app');
+        }
     }
 })
