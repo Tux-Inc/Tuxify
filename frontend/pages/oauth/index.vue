53775e69e7d64345b704b6228ca40230254241e6
Neeptossss <gwenael.hubler@epitech.eu>
2023-10-17 14:46:00 +0200

    feat(pages): refacto service request & spinner loader for redirect page

--- frontend/pages/oauth/index.vue
+++ frontend/pages/oauth/index.vue
@@ -17,9 +17,11 @@ async function getUser() {
             title: `Error ${error.value.statusCode}`,
             description: error.value.message,
         });
+        await router.push("/auth/sign-in");
     } else {
         toast.add({
             color: "green",
+            icon: "i-heroicons-check-circle",
             title: "Success",
             description: "You are now logged in",
         });
@@ -36,9 +38,39 @@ async function getUser() {
 getUser();
 </script>
 
+<style scoped>
+.loader {
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    position: relative;
+    animation: rotate 1s linear infinite
+}
+.loader::before {
+    content: "";
+    box-sizing: border-box;
+    position: absolute;
+    inset: 0px;
+    border-radius: 50%;
+    border: 5px solid #FFF;
+    animation: prixClipFix 2s linear infinite ;
+}
+
+@keyframes rotate {
+    100%   {transform: rotate(360deg)}
+}
+
+@keyframes prixClipFix {
+    0%   {clip-path:polygon(50% 50%,0 0,0 0,0 0,0 0,0 0)}
+    25%  {clip-path:polygon(50% 50%,0 0,100% 0,100% 0,100% 0,100% 0)}
+    50%  {clip-path:polygon(50% 50%,0 0,100% 0,100% 100%,100% 100%,100% 100%)}
+    75%  {clip-path:polygon(50% 50%,0 0,100% 0,100% 100%,0 100%,0 100%)}
+    100% {clip-path:polygon(50% 50%,0 0,100% 0,100% 100%,0 100%,0 0)}
+}
+</style>
+
 <template>
     <div class="h-screen w-full flex flex-col items-center justify-center">
-        <UIcon name="i-heroicons-arrow-path" class="w-12 h-12" />
-        <h1 class="text-3xl font-bold mt-2">Redirecting to the app...</h1>
+        <span class="loader"></span>
     </div>
 </template>
