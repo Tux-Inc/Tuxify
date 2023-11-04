6f1749b11dc5ae768ce27e2d78746db7d499f5f6
AlexandreDFM <alexandre-kevin.de-freitas-martins@epitech.eu>
2023-11-04 22:59:02 +0100

    refactor(files): headers & format mailer service

--- backend/mailer/src/event/user-reset-password.event.ts
+++ backend/mailer/src/event/user-reset-password.event.ts
@@ -1,4 +1,40 @@
+/*
+File Name: user-reset-password.event.ts
+Author: Gwenaël Hubler, Stephane Fievez, Roman Lopes, Alexandre Kévin De Freitas Martins, Bouna Diallo
+Creation Date: 2023
+Description: Event for user reset password file
+Copyright (c) 2023 Tux Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the 'Software'), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+/* The UserResetPasswordEvent class represents an event where a user requests to
+reset their password and contains the user's email and reset link. */
 export class UserResetPasswordEvent {
+    /**
+     * The constructor function takes in an email and a reset link as parameters
+     * and assigns them to the corresponding properties of the class.
+     * @param {string} email - A string representing the email address of the
+     * user.
+     * @param {string} resetLink - A string representing the reset link for the
+     * user's account.
+     */
     constructor(
         public readonly email: string,
         public readonly resetLink: string,
--- backend/mailer/src/mailer-wrapper.controller.spec.ts
+++ backend/mailer/src/mailer-wrapper.controller.spec.ts
@@ -1,14 +1,42 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { MailerWrapperController } from './mailer-wrapper.controller';
+/*
+File Name: mailer-wrapper.controller.spec.ts
+Author: Gwenaël Hubler, Stephane Fievez, Roman Lopes, Alexandre Kévin De Freitas Martins, Bouna Diallo
+Creation Date: 2023
+Description: Test for mailer wrapper controller file
+Copyright (c) 2023 Tux Inc.
 
-describe('AppController', () => {
-  let appController: MailerWrapperController;
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the 'Software'), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [MailerWrapperController],
-    }).compile();
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
 
-    appController = app.get<MailerWrapperController>(MailerWrapperController);
-  });
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+import { Test, TestingModule } from "@nestjs/testing";
+import { MailerWrapperController } from "./mailer-wrapper.controller";
+
+describe("AppController", () => {
+    let appController: MailerWrapperController;
+
+    beforeEach(async () => {
+        const app: TestingModule = await Test.createTestingModule({
+            controllers: [MailerWrapperController],
+        }).compile();
+
+        appController = app.get<MailerWrapperController>(
+            MailerWrapperController,
+        );
+    });
 });
