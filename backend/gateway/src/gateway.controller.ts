e4bf11a9063d54ceb6d06355a35706390d6ee901
Neeptossss <gwenael.hubler@epitech.eu>
2023-11-06 00:10:21 +0100

    feat(chore): add about.json

--- backend/gateway/src/gateway.controller.ts
+++ backend/gateway/src/gateway.controller.ts
@@ -1,12 +1,29 @@
-import {Body, Controller, Post} from '@nestjs/common';
+import {Body, Controller, Get, Inject, Post, Req} from '@nestjs/common';
 import {GatewayService} from "./gateway.service";
 import {CreatedUserDto} from "./created-user.dto";
+import {lastValueFrom} from "rxjs";
+import {ClientProxy} from "@nestjs/microservices";
 
 @Controller()
 export class GatewayController {
     constructor(
         private readonly gatewayService: GatewayService,
+        @Inject('NATS_CLIENT') private readonly natsClient: ClientProxy,
     ) {}
+
+    @Get('/about.json')
+    async sendAboutJson(@Req() req: any) {
+        return {
+            client: {
+                host: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+            },
+            server: {
+                current_time: new Date().toISOString(),
+                services: await lastValueFrom(this.natsClient.send('infos.providers', {})),
+            },
+        };
+    }
+
 
     @Post()
     sendEmail(@Body() createdUserDto: CreatedUserDto) {
